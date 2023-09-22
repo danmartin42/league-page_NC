45abffda19e6f994229c9a62ba5a7aadcae87538
danmartin42 <69535489+danmartin42@users.noreply.github.com>
2023-09-22 15:07:36 -0500

    Update leagueInfo.js

--- src/lib/utils/leagueInfo.js
+++ src/lib/utils/leagueInfo.js
@@ -1,5 +1,5 @@
 /*   STEP 1   */
-export const leagueID = "821132527627505664"; // your league ID
+export const leagueID = "922945173359845376"; // your league ID
 export const leagueName = "Boobs League"; // your league name
 export const dues = 100; // (optional) used in template constitution page
 export const dynasty = true; // true for dynasty leagues, false for redraft and keeper
